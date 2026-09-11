e5d70438a04eea77b5af3286ad8a907ff345333a
Daniel <dc9090-web@users.noreply.github.com>
2026-09-11 20:38:52 +1000

    chore: default log filter hides third-party theme noise (warn, yutani=info)

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() -> ExitCode {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("warn,yutani=info,cosmic::theme=off,cosmic::app::cosmic=off,cosmic::app=error")),
         )
         .with_writer(std::io::stderr)
         .init();
